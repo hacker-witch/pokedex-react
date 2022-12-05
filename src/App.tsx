780a804296ad3ddda7806087c8cc5412a2a87fad
Samantha Santos Pereira <samantha.sant65@gmail.com>
2022-12-05 16:54:21 -0300

    Add some mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,31 @@
 function App() {
+  const pokemonSpeciesList = [
+    {
+      nationalPokedexEntryNumber: 1,
+      name: 'bulbasaur',
+      types: ['grass', 'poison'],
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+    },
+    {
+      nationalPokedexEntryNumber: 2,
+      name: 'ivysaur',
+      types: ['grass', 'poison'],
+      imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"
+    },
+    {
+      nationalPokedexEntryNumber: 3,
+      name: 'venusaur',
+      types: ['grass', 'poison'],
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png'
+    },
+    {
+      nationalPokedexEntryNumber: 4,
+      name: 'charmander',
+      types: ['fire'],
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+    }
+  ]
+
   return (
     <main className="app">
       <h1 className="app__title">Pokédex</h1>
