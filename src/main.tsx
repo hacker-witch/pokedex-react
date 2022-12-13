4788a78a45d22305b1457a7689448a4fb08b4aac
Samantha Santos Pereira <samantha.sant65@gmail.com>
2022-12-13 00:20:20 -0300

    Render root route instead of app component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RootRoute } from "./routes/RootRoute";
 import "./index.scss";
 
 const queryClient = new QueryClient({
@@ -13,10 +14,17 @@ const queryClient = new QueryClient({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: "/pokedex-react",
+    element: <RootRoute />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
