b77ef4f786070fd2d816ac90560150482721176e
Samantha Santos Pereira <samantha.sant65@gmail.com>
2022-12-13 00:32:47 -0300

    Persist ache locally again with a temporary  random cache buster

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { get, set, del } from "idb-keyval";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootRoute } from "./routes/RootRoute";
 import "./index.scss";
+import { PersistedClient } from "@tanstack/react-query-persist-client";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,6 +18,21 @@ const queryClient = new QueryClient({
   },
 });
 
+const idbKey = "react-query";
+const persister = {
+  persistClient: async (client: PersistedClient) => {
+    set(idbKey, client);
+  },
+  restoreClient: async () => {
+    return await get<PersistedClient>(idbKey);
+  },
+  removeClient: async () => {
+    await del(idbKey);
+  },
+};
+
+const cacheBuster = Math.random().toString();
+
 const router = createBrowserRouter([
   {
     path: "/pokedex-react",
@@ -24,9 +42,12 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister, buster: cacheBuster }}
+    >
       <RouterProvider router={router} />
       <ReactQueryDevtools />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </React.StrictMode>
 );
